a7f5dd5fafc2258a39d27cf4d40e60b5a3934c44
benasval <benas.valaitis6@gmail.com>
2023-03-27 08:09:37 +0200

    .

--- music/mtunes/views.py
+++ music/mtunes/views.py
@@ -67,36 +67,6 @@ def history(request):
 
     return render(request, 'mtunes/history.html', {"history": song})
 
-def login(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        login(request, user)   
-        redirect("/")
-
-    return render(request, 'mtunes/login.html')
-
-def signup(request):
-    if request.method == "POST":
-        email = request.POST['email']
-        username = request.POST['username']
-        first_name = request.POST['firstname']
-        last_name = request.POST['lastname']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
-
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = first_name
-        myuser.last_name = last_name
-        myuser.save()
-        user = authenticate(username=username, password=pass1)
-        login(request, user)
-
-        return redirect('/')
-
-    return render(request, 'mtunes/signup.html')
-
 def logout_user(request):
     logout(request)
     return redirect("/")
